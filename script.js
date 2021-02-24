41ca4da8663440a8f3aa0e7570222dad8be98662
sandermeijer1972 <69959339+sandermeijer1972@users.noreply.github.com>
2021-02-24 09:43:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// recept voor pizza
+const makePizza = function() {
+    console.log("Verwarm de oven voor op 220 graden");
+    console.log("Rol het pizzadeeg uit");
+    console.log("Besmeer het pizzadeeg met tomatensaus");
+    console.log("Beleg de pizza met broccoli, paprika, ham en pesto");
+    console.log("Bestrooi de pizza met wat kaas");
+    console.log("Bak de pizza 15-20 minuten in de oven");
+};
+// recept voor sushi
+const makeSushi = function() {
+    console.log("Kook de rijst 15 minuten");
+    console.log("Verwarm ondertussen de rijstazijn en los daarin zout en suiker op");
+    console.log("Meng de rijst en rijstazijn en laat afkoelen");
+    console.log("Leg je eerste norivel klaar");
+    console.log("Breng een laag rijst aan");
+    console.log("In het midden leg je een dubbele streep met tonijn en komkommer");
+    console.log("rol je norivel op");
+    console.log("Snij de rol in plakjes");
+};
+// bestelling
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
